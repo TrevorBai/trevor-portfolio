871c1f5a9e21575459dee3becf3fa92eabd578e5
Trevor Bai <baijin2011@gmail.com>
2021-01-31 13:31:16 -0500

    add one more Stanford class

--- src/pages/ConnectingDotsPage/index.tsx
+++ src/pages/ConnectingDotsPage/index.tsx
@@ -19,13 +19,24 @@ const ConnectingDots: FC<RouteComponentProps> = () => {
           </i>
         </p>
         <p className="courses">
-          <i>
-            ✔ &nbsp;&nbsp; MIT 6.042J Mathematics for Computer Science
-          </i>
+          <i>✔ &nbsp;&nbsp; MIT 6.042J Mathematics for Computer Science</i>
+        </p>
+        <br />
+        <hr />
+        <br />
+        <p>
+          Stanford center for professional development also offers very high
+          quality courses helping me{' '}
+          <b>CONNECT each technology I have learnt TOGETHER</b>. A list of
+          Stanford engineering courses I have taken including:
+        </p>
+        <br />
+        <p className="courses">
+          <i>✔ &nbsp;&nbsp; CS106A Programming Methodology</i>
         </p>
       </div>
     </div>
   );
-}
+};
 
 export default ConnectingDots;
