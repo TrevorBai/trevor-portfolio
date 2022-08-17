27b7f83e73f00ec4cf29f9e3da47cba2041ecbfa
Trevor Bai <baijin2011@gmail.com>
2022-08-16 21:27:16 -0400

    Mod biography

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -7,7 +7,7 @@ const MainPage = () => {
       <img className="round-img" src={avatarLarge} alt="Avatar-Large" />
       <hr />
       <h1><i>biography</i></h1>
-      <p>As a software developer, I have recently worked on windows applications using <b>c#</b> and <b>dotnet</b> framework. The applications are
+      <p>As a software developer, I have recently developed windows applications using <b>c#</b> and <b>dotnet</b> framework. The applications are
       regarding to computational geometry and 2D graphics and are solving/automating civil engineering design process.
       I am also good at building web and mobile apps using React/React Native and Typescript.</p>
     </div>
